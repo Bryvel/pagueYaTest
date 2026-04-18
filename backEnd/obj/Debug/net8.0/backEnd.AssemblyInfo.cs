bfa6fedc003bfa1ea3e3da6300b8ec300968ce3a
Bryan Velasco <bryvel_1101@yahoo.com>
2026-04-18 18:07:15 -0500

    test forntend

--- backEnd/obj/Debug/net8.0/backEnd.AssemblyInfo.cs
+++ backEnd/obj/Debug/net8.0/backEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c98dcad5a3c364446060d4adbb3fb41ba837d051")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b12603c1cb4aea34a6cf03755adba95a6943f213")]
 [assembly: System.Reflection.AssemblyProductAttribute("backEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
